db83b5909bd6eac4382ec32d73cf1a79306da883
Haktan Suren <hsuren@cpan.org>
2011-04-10 02:14:12 +0000

    Import of HSUREN/TAIR-Blast-1.01 from CPAN.
    gitpan-cpan-distribution: TAIR-Blast
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         HSUREN/TAIR-Blast-1.01.tar.gz
    gitpan-cpan-author:       HSUREN
    gitpan-cpan-maturity:     released

--- lib/TAIR/Blast.pm
+++ lib/TAIR/Blast.pm
@@ -3,97 +3,45 @@ package TAIR::Blast;
 use warnings;
 use strict;
 
+use LWP::UserAgent;
+use HTTP::Request::Common;
+	
 =head1 NAME
 
 TAIR::Blast - A module to gather automated BLAST result from TAIR (http://www.arabidopsis.org/Blast/index.jsp) 
 
 =head1 VERSION
 
-Version 1.00
+Version 1.01
 
 =cut
 
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 
 =head1 SYNOPSIS
 
 This module simply automatically! BLAST any type of sequences (nucleotide, protein) with using different type of algorithm (blastp, blastn, tblastx etc.) by using TAIR Blast engine.
-
-    #METHOD #1 for one sequence only
-    use TAIR::Blast;
-
-    my $TB = TAIR::Blast->new();
     
-    #START OF PARAMETERS FOR BLAST#
-	my $query = "estExt_fgenesh4_pm.C_1450005";
-	my $query_seq = "ATGGGAGCTGTTGCTGCGATATGGTGGTTAACGGTGGTTGCTGCTGCATCTCGCCTTTCATTCCTGCACGCCT";
-	my $algorithm = "blastn"; #other set : blastp, blastx, tblastx,tblastn
-	my $maxscore; #http://www.arabidopsis.org/help/helppages/BLAST_help.jsp#alignments (OPTIONAL #default=250)
-	my $blast_target_set; #http://www.arabidopsis.org/help/helppages/BLAST_help.jsp#datasets (OPTIONAL #default for blastn, tblastx, tblastn is At_transcripts and for blastp and blastx is ATH1_pep. You can alternatively select from the list according to algorithm)
-	#At_transcripts = TAIR10 Transcripts (-introns, +UTRs) (DNA) 
-	#ATH1_cds = TAIR10 CDS (-introns, -UTRs) (DNA) 
-	#ATH1_seq = TAIR10 Genes (+introns, +UTRs) (DNA) 
-	#ATH1_pep = TAIR10 Proteins (Protein) 
-	#ATH1_bacs_con = TAIR10 Whole Genome (BAC clones) (DNA) 
-	#At_upstream_500 = TAIR10 Loci Upstream Sequences -- 500 bp (DNA) 
-	#At_upstream_1000 = TAIR10 Loci Upstream Sequences -- 1000 bp (DNA) 
-	#At_upstream_3000 = TAIR10 Loci Upstream Sequences -- 3000 bp (DNA) 
-	#At_downstream_1000 = TAIR10 Loci Downstream Sequences -- 1000 bp (DNA) 
-	#At_downstream_3000 = TAIR10 Loci Downstream Sequences -- 3000 bp (DNA) 
-	#At_intergenic = TAIR10 Intergenic (DNA) 
-	#At_intron = TAIR10 Intron (DNA) 
-	#ATH1_5_UTR = TAIR10 5' UTRs (DNA) 
-	#ATH1_3_UTR = TAIR10 3' UTRs (DNA) 
-	#At_flanks_DNA = A. thaliana Insertion Flanks (DNA) 
-	#At_Uniprot_prot = A. thaliana UniProt (Protein) 
-	#At_GB_exp_prot = A. thaliana GB derived from mRNA (Protein) 
-	#At_GB_refseq_prot = A. thaliana GB refseq/tpa (Protein) 
-	#At_GB_all_prot = A. thaliana GB all (Protein) 
-	#At_GB_exp_tx_DNA = A. thaliana GB experimental cDNA/EST (DNA) 
-	#At_GB_refseq_tx_DNA = A. thaliana GB refseq/tpa cDNA (DNA) 
-	#At_GB_genomic_DNA = A. thaliana GB genomic (DNA) 
-	#gp_GB_exp_prot = Green plant GB derived from mRNA (Protein) 
-	#gp_GB_refseq_prot = Green plant GB refseq/tpa (Protein) 
-	#gp_GB_all_prot = Green plant GB all (Protein) 
-	#gp_GB_exp_tx_DNA = Green plant GB experimental cDNA/EST (DNA) 
-	#gp_GB_refseq_tx_DNA = Green plant GB refseq/tpa cDNA (DNA) 
-	#gp_GB_genomic_DNA = Green plant GB genomic (DNA) 
-	#END OF PARAMETERS FOR BLAST#
-	my $verbose = 1; #default (0, no info);
-	
-    my $result = $TB->connect($query,$query_seq,$algorithm,$maxscore,$blast_target_set,$verbose);
-    
-    #you would like to have the output recorded!
-    my $output_file = "output_file.txt";
-    $TB->write($output_file);
-    
-    
-    #METHOD #2 for multiple sequences with the help of Bio::DB::Fasta 
-    use TAIR::Blast;
-	use Bio::DB::Fasta; #You may use different but it is good for parsing your fasta file!
+	use TAIR::Blast;
+	use Bio::DB::Fasta;
 	
 	my $TB = TAIR::Blast->new();
 	
 	my $fasta_file = "seqs.fasta";
-	
 	my $algorithm = "blastn"; #other set : blastp, blastx, tblastx,tblastn
-	my $maxscore; #http://www.arabidopsis.org/help/helppages/BLAST_help.jsp#alignments (OPTIONAL #default=250)
+	my $maxscore;
 	my $blast_target_set;
 	my $verbose = 1;
 	
 	my $stream  = Bio::DB::Fasta->new($fasta_file)->get_PrimarySeq_stream;
 	  while (my $query = $stream->next_seq) {
 		my $query_seq = $query->seq;
-		#OTHER PARAMETERS ARE SAME AS IN METHOD 2
 		my $result = $TB->connect($query,$query_seq,$algorithm,$maxscore,$blast_target_set,$verbose);
 	  }
 	my $output_file = "output_file.txt";
 	$TB->write($output_file);
 	
-	
-    ...
-
 =head1 SUBROUTINES/METHODS
 
 =head2 new
@@ -124,9 +72,6 @@ sub connect
 	my $maxscore = shift;
 	my $blast_target_set = shift;
 	my $verbose = shift;
-		
-	use LWP::UserAgent;
-	use HTTP::Request::Common;
 	
 	if (!defined($blast_target_set)) {
 		if ($algorithm =~ /^blast[px]$/i){
@@ -229,7 +174,7 @@ sub write
 
 =head1 AUTHOR
 
-Haktan Suren, << <hsuren at vt.edu> >>
+Haktan Suren, << <hsuren at cpan.org> >>
 
 =head1 BUGS
 
@@ -269,9 +214,14 @@ L<http://search.cpan.org/dist/TAIR-Blast/>
 
 =back
 
+=head1 DEPENDENCIES
 
+	LWP::UserAgent
+	HTTP::Request::Common
+	
 =head1 ACKNOWLEDGEMENTS
 
+Special thanks to LC :)
 
 =head1 LICENSE AND COPYRIGHT
 
